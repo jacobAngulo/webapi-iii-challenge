317798aa5cfa014cba4662e6df5016ed3f038b7d
Jacob Angulo <jacobangulo@hotmail.com>
2019-03-21 15:55:01 -0700

    added cors to hook up w react app

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 
-const usersRouter = require('./data/helpers/routers/usersRouter.js')
-const postsRouter = require('./data/helpers/routers/postsRouter.js')
+const usersRouter = require("./data/helpers/routers/usersRouter.js");
+const postsRouter = require("./data/helpers/routers/postsRouter.js");
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
-server.use('/api/users', usersRouter)
-server.use('/api/posts', postsRouter)
+server.use("/api/users", usersRouter);
+server.use("/api/posts", postsRouter);
 
 server.get("/", (req, res) => {
   res.send("<3");
